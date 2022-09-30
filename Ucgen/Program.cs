ff55f26a0e83c2d0b39ef2b85f22a4581fd40b37
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2022-10-01 01:59:52 +0300

    Add project files.

---
+++ Ucgen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ucgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgen için uzunluk giriniz: ");
+            int edge = int.Parse(Console.ReadLine());
+            Ucgen(edge);
+        }
+
+        static void Ucgen(int e)
+        {
+            for (int i = 0; i < e; i++)
+            {
+                for(int j = i; j < e-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
